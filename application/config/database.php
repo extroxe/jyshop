35e25051b92ef31f391f52411ac575f7161c5210
extroxe <979847073@qq.com>
2017-04-10 14:47:01 +0800

    修改数据库配置

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhos',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'sw-shines-shop',
