2f85c626b1fb8ae1c03a7815cf6275a60f91d0dd
extroxe <979847073@qq.com>
2017-04-13 11:48:46 +0800

    后台编辑报告保存出错修改

--- application/controllers/admin/Report_admin.php
+++ application/controllers/admin/Report_admin.php
@@ -92,6 +92,8 @@ class Report_admin extends CI_Controller {
         $this->form_validation->set_rules('id', '报告id', 'trim|required');
         $this->form_validation->set_rules('number', '报告编号', 'trim|required');
         $this->form_validation->set_rules('name', '姓名', 'trim|required');
+        $this->form_validation->set_rules('age', '年龄', 'trim|required|is_natural_no_zero');
+        $this->form_validation->set_rules('gender', '性别', 'trim|required|is_natural');
         $this->form_validation->set_rules('phone', '手机号码', 'trim|required|regex_match[/^1(3|4|5|7|8)\d{9}$/]');
         $this->form_validation->set_rules('identity_card', '身份证号', 'trim|required|regex_match[/^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X|x)$/]');
         $this->form_validation->set_rules('attachment_id', '报告ID', 'trim|required|is_natural_no_zero');
@@ -103,10 +105,12 @@ class Report_admin extends CI_Controller {
             $report_id = $this->input->post('id', TRUE);
             $number = $this->input->post('number', TRUE);
             $name = $this->input->post('name', TRUE);
+            $age = $this->input->post('age', TRUE);
+            $gender = $this->input->post('gender', TRUE);
             $phone = $this->input->post('phone', TRUE);
             $identity_card = $this->input->post('identity_card', TRUE);
             $attachment_id = $this->input->post('attachment_id', TRUE);
-            $result = $this->Report_model->update($report_id, $number, $name, $phone, $identity_card, $attachment_id);
+            $result = $this->Report_model->update($report_id, $number, $name, $age, $gender, $phone, $identity_card, $attachment_id);
         }else {
             $result['error'] = $res['msg'];
             $result['msg'] = '参数错误';
